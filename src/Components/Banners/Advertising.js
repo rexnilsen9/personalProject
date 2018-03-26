634e322588c3ccf58756882b7ef103debc386358
rex nilsen <rexpnilsen9@gmail.com>
2018-03-26 14:23:59 -0600

    auth0

--- src/Components/Banners/Advertising.js
+++ src/Components/Banners/Advertising.js
@@ -13,7 +13,7 @@ export default function Advertising() {
                 <img src="https://images.unsplash.com/photo-1520443819063-a64183e22528?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b7df94d5fde27c8f6f69ec38280947c8&auto=format&fit=crop&w=1350&q=80" alt="" />
             </div>
             <div className='pic'>
-                <img src="https://images.unsplash.com/photo-1520443819063-a64183e22528?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b7df94d5fde27c8f6f69ec38280947c8&auto=format&fit=crop&w=1350&q=80" alt="" />
+                <img src="https://images.unsplash.com/photo-1506318164473-2dfd3ede3623?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d7aca7e915a70f426aa75aacaa56c6d0&auto=format&fit=crop&w=1500&q=80" alt="" />
             </div>
         </div>
 
